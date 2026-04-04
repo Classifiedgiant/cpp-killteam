7fb6cf80824c3c00310d95edd7be7dd1a1c9e3b9
Thomas Lockie <thomas_lockie@yahoo.com>
2026-04-04 06:00:44 +0100

    feat: Add drogon calls to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <drogon/drogon.h>
 
 int main()
 {
     std::cout << "Starting the server...\n";
+    drogon::app()
+    .setLogPath("./")
+    .setLogLevel(trantor::Logger::kWarn)
+    .addListener("0.0.0.0", 1337)
+    .setThreadNum(16)
+    .run();
+
     return 0;
 }

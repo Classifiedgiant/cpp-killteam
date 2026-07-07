560fa56eeccc217c38b670c797544529150a0750
Thomas Lockie <thomas_lockie@yahoo.com>
2026-07-07 11:06:22 +0100

    feat: Add pre and post flight CORS checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <regex>
 
 #include <drogon/drogon.h>
 
@@ -9,6 +10,21 @@
 
 
 std::unique_ptr<Game::Game> game = std::make_unique<Game::Game>();
+
+namespace
+{
+    const std::regex localhostOriginRegex(R"(^http://localhost(:\d+)?$)");
+
+    std::string allowedOriginFor(const drogon::HttpRequestPtr &req)
+    {
+        const auto &origin = req->getHeader("Origin");
+        if (std::regex_match(origin, localhostOriginRegex))
+        {
+            return origin;
+        }
+        return "http://localhost";
+    }
+}
 
 int main()
 {
@@ -38,6 +54,29 @@ int main()
 
     drogonApp.registerWebSocketController("/game", "Controllers::GameWsController");
     drogonApp.registerWebSocketControllerRegex("/game/[^/]*", "Controllers::GameWsController");
+
+
+    // Add cors pre and postflight checks
+    drogonApp.registerPreRoutingAdvice(
+        [](const drogon::HttpRequestPtr &req,
+        drogon::AdviceCallback &&callback,
+        drogon::AdviceChainCallback &&chainCallback) {
+            if (req->method() == drogon::Options)
+            {
+                auto resp = drogon::HttpResponse::newHttpResponse();
+                resp->addHeader("Access-Control-Allow-Origin", allowedOriginFor(req));
+                resp->addHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+                resp->addHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+                callback(resp);
+                return;
+            }
+            chainCallback();
+        });
+
+    drogonApp.registerPostHandlingAdvice(
+        [](const drogon::HttpRequestPtr &req, const drogon::HttpResponsePtr &resp) {
+            resp->addHeader("Access-Control-Allow-Origin", allowedOriginFor(req));
+        });
 
     drogonApp.run();
 
